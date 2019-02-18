1e040b32fdb0d274251e3409001d656d3ad2ddf9
Dawid Dyczek <wizard91@o2.pl>
2019-02-18 23:57:17 +0000

    update

--- src/components/chart.js
+++ src/components/chart.js
@@ -11,9 +11,9 @@ export default (props) => {
   return (
   <div>
     <Sparklines height={40} width={90} data={props.data}>
-                <SparklinesLine color ={props.color} />
-                <SparklinesReferenceLine type="avg" />
-                </Sparklines>
+      <SparklinesLine color ={props.color} />
+      <SparklinesReferenceLine type="avg" />
+      </Sparklines>
                 <div>{average(props.data)}{props.units}</div>
     </div>
     );
