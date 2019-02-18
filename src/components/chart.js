86f99451bddc92742cd8ece479ffa319eab7b7b7
Dawid Dyczek <wizard91@o2.pl>
2019-02-18 23:59:02 +0000

    update

--- src/components/chart.js
+++ src/components/chart.js
@@ -3,18 +3,19 @@ import React from 'react';
 import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 function average(data){
-    return _.round(_.sum(data)/data.length);
-
-    
+  return _.round(_.sum(data)/data.length);
 }
+
 export default (props) => {
   return (
-  <div>
-    <Sparklines height={40} width={90} data={props.data}>
+    <div>
+      <Sparklines height={40} width={90} data={props.data}>
       <SparklinesLine color ={props.color} />
       <SparklinesReferenceLine type="avg" />
       </Sparklines>
-                <div>{average(props.data)}{props.units}</div>
+      <div>
+        {average(props.data)}{props.units}
+      </div>
     </div>
-    );
+  );
 }
