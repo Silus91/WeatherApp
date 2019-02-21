c709bc5a858bf8f9e1e6764dc7a69c5112191bcd
Dawid Dyczek <wizard91@o2.pl>
2019-02-21 22:49:39 +0000

    update

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -4,13 +4,12 @@ import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index';
 
 class SearchBar extends Component {
-    constructor(props){
-        super(props);
-        this.state = { term: ''};
-
-        this.onInputChange= this.onInputChange.bind(this);
-        this.onFormSubmit = this.onFormSubmit.bind(this);
-    }
+  constructor(props){
+    super(props);
+    this.state = { term: ''};
+    this.onInputChange= this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
 
     onInputChange(event) {
         console.log(event.target.value);
