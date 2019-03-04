bf0e9326c998c5f3b5196f69390e55dd88535ca0
Dawid Dyczek <wizard91@o2.pl>
2019-03-04 11:59:49 +0000

    small fixes

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,8 +9,8 @@ export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
     
-    return {
-        type: FETCH_WEATHER,
-        payload: request
-    };
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
 }
