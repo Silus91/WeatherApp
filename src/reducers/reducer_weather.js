ca2e166d0acbe3a50e9ed35defd37d4696c542aa
Dawid Dyczek <wizard91@o2.pl>
2019-03-05 09:59:42 +0000

    small fixes

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -2,9 +2,9 @@ import { FETCH_WEATHER } from '../actions/index';
 
 export default function (state = [], action){
   switch (action.type){
-        case FETCH_WEATHER:
-    return state.concat([action.payload.data]);
-    }
+    case FETCH_WEATHER:
+  return state.concat([action.payload.data]);
+  }
     
-    return state;
+  return state;
 }
