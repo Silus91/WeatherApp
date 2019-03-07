e8825ecc23fa75d09fceb7c7770236a6cab6101f
Dawid Dyczek <wizard91@o2.pl>
2019-03-07 07:59:29 +0000

    small fixes

--- src/components/chart.js
+++ src/components/chart.js
@@ -10,7 +10,7 @@ export default (props) => {
   return (
     <div>
           <Sparklines height={40} width={90} data={props.data}>
-        <SparklinesLine color ={props.color} />
+          <SparklinesLine color ={props.color} />
         <SparklinesReferenceLine type="avg" />
       </Sparklines>
       <div>
