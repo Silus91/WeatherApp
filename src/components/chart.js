322fb4babd214d3d1dd036b6242c5bcc6e6d8ade
Dawid Dyczek <wizard91@o2.pl>
2019-03-07 07:59:05 +0000

    small fixes

--- src/components/chart.js
+++ src/components/chart.js
@@ -9,7 +9,7 @@ function average(data){
 export default (props) => {
   return (
     <div>
-      <Sparklines height={40} width={90} data={props.data}>
+          <Sparklines height={40} width={90} data={props.data}>
         <SparklinesLine color ={props.color} />
         <SparklinesReferenceLine type="avg" />
       </Sparklines>
